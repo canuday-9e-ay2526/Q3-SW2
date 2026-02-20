beb1a51bf83ab1c2b026e6d315d8a19f27b870a6
canuday-9e-ay2526 <canuday.ja@obmontessori.edu.ph>
2026-02-20 11:28:10 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+//function for discount button
+function buttonDiscount (){
+    let budgetentree = document.getElementById(phpbudget).value;
+    let disc = Number(budgetentree*0.2);
+
+    document.getElementById().innetHTML=
+    <div class="container2 bg-secondary rounded mt-5">
+    <h4>Recommended Brands</h4>
+    <button class="btn btn-danger mt-3">Acer</button>
+    <button class="btn btn-danger mt-3">Asus</button>
+    <button class="btn btn-danger mt-3">Lenovo</button>
+
+    <p class="text-muted">Elligable discount</p>
+
+  </div>;
+    
+}
